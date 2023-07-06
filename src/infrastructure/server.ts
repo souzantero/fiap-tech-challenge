0a9ffdb784aadc6aa5f98f33a3ea932cbbdde04d
Felipe Antero <souzantero@gmail.com>
2023-07-06 14:28:06 -0300

    feat(configuration): write configuration file

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,11 +1,11 @@
 import { App } from './app';
 import { PrismaDatabase } from './databases/prisma/prisma-database';
+import { environment } from './configuration/environment';
 
-const port = process.env.PORT || 3000;
 const app = App.create(new PrismaDatabase());
 
-if (process.env.NODE_ENV === 'development') {
+if (environment.name === 'development') {
   app.swagger();
 }
 
-app.start(+port);
+app.start(environment.port);
